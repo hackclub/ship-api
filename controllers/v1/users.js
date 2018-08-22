aa7d00c1a4a4967c622784c1766787955d3e2884
Victor Truong <me@ifvictr.com>
2018-08-21 22:17:27 -0700

    Add GET /v1/users/:id and GET /v1/users/username/:username

--- controllers/v1/users.js
+++ controllers/v1/users.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const passport = require('passport')
+const { User } = require('../../models')
 
 const router = express.Router()
+
+router.route('/:id')
+    .get((req, res) => {
+        User.findById(req.params.id).then(user => {
+            if (user) {
+                res.json(user)
+            }
+            else {
+                res.status(404).json({ message: 'user not found' })
+            }
+        })
+    })
 
 router.route('/auth/github')
     .get(passport.authenticate('github', { scope: ['user:email'] }))
@@ -32,6 +45,18 @@ router.route('/current')
             return
         }
         res.json(req.user)
+    })
+
+router.route('/username/:username')
+    .get((req, res) => {
+        User.findOne({ where: { username: req.params.username } }).then(user => {
+            if (user) {
+                res.json(user)
+            }
+            else {
+                res.status(404).json({ message: 'user not found' })
+            }
+        })
     })
 
 module.exports = router
