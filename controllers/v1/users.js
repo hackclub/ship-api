9ba13cb7f7815f60b75be6624576664e6b23ec34
Victor Truong <me@ifvictr.com>
2018-12-29 15:01:27 -0800

    Return auth token when successfully authed

--- controllers/v1/users.js
+++ controllers/v1/users.js
@@ -1,8 +1,66 @@
+const crypto = require('crypto')
 const express = require('express')
 const passport = require('passport')
 const { ProjectImage, ProjectLink, Topic, User } = require('../../models')
 
 const router = express.Router()
+
+router.route('/auth/github')
+    .get(passport.authenticate('github', { scope: ['user:email'] }))
+
+router.route('/auth/github/callback')
+    .get(
+        passport.authenticate('github', { failureRedirect: `${process.env.SITE_URL}/login?status=failed` }),
+        (req, res) => {
+            const authToken = crypto.randomBytes(32).toString('hex')
+            const data = {
+                auth_token: authToken,
+                auth_token_created_at: new Date().toISOString()
+            }
+            User.update(data, { where: { id: req.user.id } }).then(() => {
+                res.redirect(`${process.env.SITE_URL}/login?status=success&auth_token=${authToken}`)
+            })
+        }
+    )
+
+router.route('/auth/slack')
+    .get(passport.authenticate('slack'))
+
+router.route('/auth/slack/callback')
+    .get(
+        passport.authenticate('slack', { failureRedirect: `${process.env.SITE_URL}/login?status=failed` }),
+        (req, res) => {
+            const authToken = crypto.randomBytes(32).toString('hex')
+            const data = {
+                auth_token: authToken,
+                auth_token_created_at: new Date().toISOString()
+            }
+            User.update(data, { where: { id: req.user.id } }).then(() => {
+                res.redirect(`${process.env.SITE_URL}/login?status=success&auth_token=${authToken}`)
+            })
+        }
+    )
+
+router.route('/current')
+    .get((req, res) => {
+        if (!req.user) {
+            res.status(401).json({ message: 'authorization required' })
+            return
+        }
+        res.json(req.user)
+    })
+
+router.route('/username/:username')
+    .get((req, res) => {
+        User.findOne({ where: { username: req.params.username } }).then(user => {
+            if (user) {
+                res.json(user)
+            }
+            else {
+                res.status(404).json({ message: 'user not found' })
+            }
+        })
+    })
 
 router.route('/:id')
     .get((req, res) => {
@@ -52,49 +110,6 @@ router.route('/:id/projects')
                     res.status(404).json({ message: 'user not found' })
                 }
             })
-    })
-
-router.route('/auth/github')
-    .get(passport.authenticate('github', { scope: ['user:email'] }))
-
-router.route('/auth/github/callback')
-    .get(
-        passport.authenticate('github', { failureRedirect: `${process.env.SITE_URL}/login` }),
-        (req, res) => {
-            res.redirect(process.env.SITE_URL)
-        }
-    )
-
-router.route('/auth/slack')
-    .get(passport.authenticate('slack'))
-
-router.route('/auth/slack/callback')
-    .get(
-        passport.authenticate('slack', { failureRedirect: `${process.env.SITE_URL}/login` }),
-        (req, res) => {
-            res.redirect(process.env.SITE_URL)
-        }
-    )
-
-router.route('/current')
-    .get((req, res) => {
-        if (!req.user) {
-            res.status(401).json({ message: 'authorization required' })
-            return
-        }
-        res.json(req.user)
-    })
-
-router.route('/username/:username')
-    .get((req, res) => {
-        User.findOne({ where: { username: req.params.username } }).then(user => {
-            if (user) {
-                res.json(user)
-            }
-            else {
-                res.status(404).json({ message: 'user not found' })
-            }
-        })
     })
 
 module.exports = router
